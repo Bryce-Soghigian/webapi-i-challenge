aafab0605d59f17d3d4452221e70a450523675a6
Bryce-Soghigian <brybrycodingguy@gmail.com>
2019-08-06 00:50:25 -0500

    Trying to further understand promises

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ const port = 5555;
 //Write out the following endpoints
 //POST /api/users
 //GET /api/users
-//GET /api/users/:import { render } from 'react-dom'
+//GET /api/users/:id
 //DELETE /api/users/id
 //put /api/users/id
 
-//========================\\\\\\
+//========================\\
 //Get requests
 
 server.get("/api/users", (request, response) => {
@@ -28,6 +28,94 @@ server.get("/api/users", (request, response) => {
         response.status(500).json({ success: false, err });
       });
   });
+//   function findById(id) {
+//     return db('users')
+//       .where({ id: Number(id) })
+//       .first();
+//   }
+server.get("/api/users/:id", (req,res) => {
+const {id} = req.params;
+console.log(id)
+    db.findById(id)
+        .then(user => {
+         if(user){
+             res.status(200).json({user});
+         }else {
+            res
+              .status(400)
+              .json({ error: "The id:"+`${id}`+ "  does not exist." });
+          }
+        })
+        .catch(err => {
+            response
+              .status(500)
+              .json({ error: "An error occured getting the user info" });
+          });
+
+
+})
+//=====================================//
+
+//Post Requests
+
+server.post("/api/users", (req, res) => {
+    const { name, bio } = req.body;
+    if (!name || !bio) {
+      return res
+        .status(400)
+        .json({ error: "YO BRO , run me that name or bio" });
+    }
+    db.insert({ name, bio })
+      .then(res => {
+        const { id } = res;
+        db.findById(id).then(user => {
+          res.status(200).json(user);
+        });
+      })
+      .catch(err => {
+        res
+          .status(400)
+          .json({ errorMessage: "Whats your name cutie ;)" });
+      });
+  });
+
+//============================//
+// function update(id, user) {
+//     return db('users')
+//       .where('id', Number(id))
+//       .update(user);
+//   }
+  server.put('api/users/:id', (req,res) => {
+    const {id, user} = req.body;
+    db.update({id, user}).then(user => {
+        //some code
+    }
+
+    ).catch(res => {
+        res.json({errorMessage:"Hey you failure"})
+    });
+
+  })
+//===========================//
+//Delete-from solution
+server.delete('/api/users/:id', (req, res) => {
+    Users.remove(req.params.id)
+      .then(count => {
+        if (count && count > 0) {
+          res.status(200).json({
+            message: 'Bye Bye',
+          });
+        } else {
+          res
+            .status(404)
+            .json({ message: 'Doesnt believe' });
+        }
+      })
+      .catch(() => {
+        res.status(500).json({ errorMessage: 'The user could not be removed' });
+      });
+  });
+  
 
 
 
